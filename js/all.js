064e1b31a6e954cea4ebbe8e5d5673f17781b694
wwchen <wwchen412@gmail.com>
2018-01-17 15:23:52 +0800

    刪除購物車

--- js/all.js
+++ js/all.js
@@ -216,22 +216,21 @@ $('input#member').on('change', function () {
                     <div class="input-group">
                     <input id="user-pwd-input" type="password" placeholder="請輸入密碼" required>
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="圖層_1" x="0px" y="0px" viewBox="0 0 30 30" style="enable-background:new 0 0 30 30;" xml:space="preserve" class="svg icon replaced-svg">
-                    <style type="text/css">
+                      <style type="text/css">
                       .st0{fill:#FFD732;}
-                    </style>
-                    <title>password</title>
-                    <g id="圖層_2">
-                      <g id="圖層_1-2">
-                        <path class="st0" d="M15,30C6.7,30,0,23.3,0,15S6.7,0,15,0s15,6.7,15,15S23.3,30,15,30z M15,1C7.3,1,1,7.3,1,15s6.3,14,14,14    s14-6.3,14-14S22.7,1,15,1z"></path>
-                        <path class="st0" d="M19.8,12.9h-1.5v-1.2c0-2.4-1.1-3.8-3.3-3.8s-3.3,1.4-3.3,3.8v1.2H10c-0.5,0.1-0.8,0.5-0.8,1v6.5    c0,0.5,0.4,0.9,0.8,1.1l1,0.3c0.5,0.2,1.1,0.2,1.6,0.3h4.8c0.5,0,1-0.1,1.5-0.2l1-0.3c0.5-0.2,0.8-0.6,0.8-1.1v-6.5    C20.7,13.4,20.3,13,19.8,12.9z M13.3,11.4c-0.1-0.9,0.5-1.7,1.4-1.8c0.1,0,0.2,0,0.3,0c0.9,0,1.6,0.6,1.7,1.5c0,0.1,0,0.2,0,0.3    v1.5h-3.3V11.4z"></path>
+                      </style>
+                      <title>password</title>
+                      <g id="圖層_2">
+                        <g id="圖層_1-2">
+                           <path class="st0" d="M15,30C6.7,30,0,23.3,0,15S6.7,0,15,0s15,6.7,15,15S23.3,30,15,30z M15,1C7.3,1,1,7.3,1,15s6.3,14,14,14    s14-6.3,14-14S22.7,1,15,1z"></path>
+                          <path class="st0" d="M19.8,12.9h-1.5v-1.2c0-2.4-1.1-3.8-3.3-3.8s-3.3,1.4-3.3,3.8v1.2H10c-0.5,0.1-0.8,0.5-0.8,1v6.5    c0,0.5,0.4,0.9,0.8,1.1l1,0.3c0.5,0.2,1.1,0.2,1.6,0.3h4.8c0.5,0,1-0.1,1.5-0.2l1-0.3c0.5-0.2,0.8-0.6,0.8-1.1v-6.5    C20.7,13.4,20.3,13,19.8,12.9z M13.3,11.4c-0.1-0.9,0.5-1.7,1.4-1.8c0.1,0,0.2,0,0.3,0c0.9,0,1.6,0.6,1.7,1.5c0,0.1,0,0.2,0,0.3    v1.5h-3.3V11.4z"></path>
+                        </g>
                       </g>
-                    </g>
                     </svg>
                     </div>
                     <div class="input-group">
-
-                         <input type="password" placeholder="密碼確認" class="user_pwd_again" required name="user_pwd_again">
-                       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="圖層_1" x="0px" y="0px" viewBox="0 0 30 30" style="enable-background:new 0 0 30 30;" xml:space="preserve" class="svg icon replaced-svg">
+                        <input type="password" placeholder="密碼確認" class="user_pwd_again" required name="user_pwd_again">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="圖層_1" x="0px" y="0px" viewBox="0 0 30 30" style="enable-background:new 0 0 30 30;" xml:space="preserve" class="svg icon replaced-svg">
                           <style type="text/css">
                             .st0{fill:#FFD732;}
                           </style>
@@ -243,7 +242,24 @@ $('input#member').on('change', function () {
                             </g>
                           </g>
                         </svg>
-                     </div>
+                    </div>
                             `
   $('.send-form .member').append(appendText);
 })
+
+ // 刪除商品
+ var removeBtn = document.querySelectorAll(".form-list .close");
+ // console.log(removeBtn);
+ for (i = 0; i < removeBtn.length; i++) {
+   removeBtn[i].addEventListener("click", function() {
+     var formList = document.querySelectorAll(".form-list");
+     // console.log(formList.length)
+     if (formList.length <= 1) {
+       $(".formLists").html('<h3 style="padding:30px 0;text-align:center;color:#aaa">目前購物車內沒有商品</h3 style="padding:30px 0;text-align:center;color:#aaa">');
+     } else {
+       $(this)
+         .closest(".form-list")
+         .remove();
+     }
+   });
+ }
